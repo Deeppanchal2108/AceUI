40a690bed0da73655ebbf8ed6e5589c9e419da63
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-06-24 14:10:16 +0530

    Finally able to render the mdx and fixed an issue

--- app/(docs)/components/[[...slug]]/page.tsx
+++ app/(docs)/components/[[...slug]]/page.tsx
@@ -1,19 +1,37 @@
 import { notFound } from "next/navigation";
 import { allDocs } from "content-collections"; // ✅ must be destructured
 import { Mdx } from "@/components/mdx-components";
+import type { Metadata } from "next";
 
-export default function TestComponentPage() {
-  const doc = allDocs.find((d) => d.slug === "text-scramble"); // 👈 match the slug
 
-  if (!doc || !doc.published) return notFound(); // ✅ correct way
+interface DocPageProps {
+  params: {
+    slug: string[];
+  };
+}
+async function getDocFromParams(props: DocPageProps) {
+  const slug = props.params.slug?.join("/") || "index";
+
+  const doc = allDocs.find((doc) => {
+    const slugParts = doc.slug.split("/"); // e.g. ["components", "text-scramble"]
+    return slugParts[slugParts.length - 1] === slug;
+  });
+
+  return doc || null;
+}
+
+export default async function ComponentDocPage(props: DocPageProps) {
+  const doc = await getDocFromParams(props);
+
+  if (!doc || !doc.published) {
+    notFound();
+  }
 
   return (
-      <div className="max-w-4xl mx-auto py-10">
-          hello inside the page.tsx file
+    <div className="max-w-4xl mx-auto py-10">
       <h1 className="text-4xl font-bold">{doc.title}</h1>
       <p className="text-muted-foreground mb-4">{doc.description}</p>
-
-      {/* <Mdx code={doc.body.code} /> */}
+      
     </div>
   );
 }
