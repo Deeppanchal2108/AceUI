8cab13efe00c767f4f27236e5bc2eb35a7cbc0f4
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-07-10 01:17:20 +0530

    sleep time

--- components/show-pattern.tsx
+++ components/show-pattern.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+interface PatternProps{
+    pattern: string | null,
+    setPattern: (pattern: string) => void
+}
+function ShowPattern({ pattern, setPattern }: PatternProps
+    
+) {
+    const [copied, setCopied] = useState<string | null>(null);
+    const [activeTab, setActiveTab] = useState<string>("all");
 
-function ShowPattern() {
+    
+
+
   return (
     <div>
       
